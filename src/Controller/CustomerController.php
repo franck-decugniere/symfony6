2e9e09bd8452ce3d0227db989322068e5351cdb4
Franck <franck.decugniere@gmail.com>
2023-11-22 18:23:20 +0100

    Récupération des données soumises

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -4,15 +4,26 @@ namespace App\Controller;
 
 use App\Form\CustomerType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Customer;
+
 
 class CustomerController extends AbstractController
 {
     #[Route('/form/customer', name: 'form_customer')]
-    public function index()
+    public function index(Request $request)
     {
-        $customerForm = $this->createForm(CustomerType::class);
+        $customer = new Customer();
+        $customerForm = $this->createForm(CustomerType::class, $customer);
+
+        $customerForm->handleRequest($request);
+        if ($customerForm->isSubmitted() && $customerForm->isValid()) 
+        {
+            // afficher les informations du formulaire
+            dump($request->request->all());
+        }
+
         return $this->render('customer/index.html.twig', [
             'customerForm' => $customerForm->createView() 
         ]);
