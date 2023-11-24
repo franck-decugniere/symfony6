4ea8eadf3431d3217348905d5aca1bd533322774
Franck <franck.decugniere@gmail.com>
2023-11-24 15:11:32 +0100

    Create/Update/Delete  via EntityManager, Select via Repository

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -7,12 +7,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Customer;
-
+use Doctrine\Persistence\ManagerRegistry;
 
 class CustomerController extends AbstractController
 {
     #[Route('/form/customer', name: 'form_customer')]
-    public function index(Request $request)
+    public function index(Request $request, ManagerRegistry $doctrine)
     {
         $customer = new Customer();
         $customerForm = $this->createForm(CustomerType::class, $customer);
@@ -22,6 +22,11 @@ class CustomerController extends AbstractController
         {
             // afficher les informations du formulaire
             dump($request->request->all());
+
+            $entityManager = $doctrine->getManager();
+            $client = $customerForm->getData();
+            $entityManager->persist($client); // prepare les données à l'insertion
+            $entityManager->flush(); // fait l'insert
         }
 
         return $this->render('customer/index.html.twig', [
